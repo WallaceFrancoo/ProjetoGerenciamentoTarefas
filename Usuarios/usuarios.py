8712746490500aa7f76aef877a3791d05a230364
Wallace Moreira Franco <009@doc.sergecont>
2025-01-28 14:20:19 -0300

    Alterando o arquivo usuarios com a utilizacao do @classmethod e o @property

--- Usuarios/usuarios.py
+++ Usuarios/usuarios.py
@@ -1,11 +1,25 @@
 class Usuarios:
-    def __init__(self,nome, departamento, senha):
-        self.nome = nome
-        self.departamento = departamento
-        self.senha = senha
+    usuarios = []
 
+    def __init__(self,nome, departamento, senha):
+        self._nome = nome
+        self._departamento = departamento
+        self._senha = senha
+        Usuarios.usuarios.append(self)
+
+    def __str__(self):
+        return self._nome
+
+    @classmethod
+    def mostrar_usuarios(cls):
+        for usuario in cls.usuarios:
+            print(f'{usuario._nome} é do departamento {usuario.departamento} a senha dele é: {usuario._senha}')
+
+    @property
+    def departamento(self):
+        print(self._departamento)
+        return 'Departamento de Tecnologia' if self._departamento == 'T.I' else 'Cadastrar departamento'
 
 usuario_wallace = Usuarios('Wallace','T.I','1188')
 usuario_daniel = Usuarios('Daniel','T.I','1188')
-print(vars(usuario_wallace))
-print(vars(usuario_daniel))
+Usuarios.mostrar_usuarios()
